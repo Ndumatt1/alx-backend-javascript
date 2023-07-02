acb8538dc0b79e67f23d6d7a2453b88b853ddfb3
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-07-02 20:14:21 +0100

    Fix error on 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      for (const value of values) {
+        if (value.status === 'rejected') {
+          value.value = `Error: ${value.reason.message}`;
+          delete value.reason;
+        }
+      }
+      return values;
+    });
 }
