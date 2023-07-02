84288887708ba247fd9cc6d0c7a0722e2f8f4b92
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-07-02 17:41:28 +0100

    fix 3-all.js and upload utils.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((values) => {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+  Promise.all([promise1, promise2]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
